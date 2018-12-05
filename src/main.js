87ec5aeb293d89eeae38caa8f6640234108e14e5
Konstantin Sharohin <ksharohin@gmail.com>
2018-12-06 00:07:44 +0200

    Big JS Update (Inputs)

--- src/main.js
+++ src/main.js
@@ -2,12 +2,8 @@ $(window).on("load", function () {
 
     let btn = $("#countForm"),
         generate = $("#generate"),
-        a1 = 1.0000000000000000000000,
-        a2 = -1.000122608749350000000000000,
-        a3 = 0.0001226087493510350000000,
         dataBase = [],
-        func,
-        t = 1 * math.exp(-10);
+        func;
 
     btn.on("click", function (event) {
         event.preventDefault();
@@ -15,42 +11,53 @@ $(window).on("load", function () {
         dataBase.length = 0;
         $(".generated").remove();
 
+        let inputL1 = $("[name=lambda1]").val(),
+            inputL2 = $("[name=lambda2]").val(),
+            inputL3 = $("[name=lambda3]").val(),
+            inputA1 = $("[name=a1]").val(),
+            inputA2 = $("[name=a2]").val(),
+            inputA3 = $("[name=a3]").val(),
+            inputT = $("[name=t]").val(),
+            inputLambdaMin = $("[name=lambdaRange]").val(),
+            setLambda1 = parseFloat(inputL1),
+            setLambda2 = parseFloat(inputL2),
+            setLambda3 = parseFloat(inputL3),
+            a1 = parseFloat(inputA1),
+            a2 = parseFloat(inputA2),
+            a3 = parseFloat(inputA3),
+            t = parseFloat(inputT),
+            lambdaMin = parseFloat(inputLambdaMin),
+            rndNumber1, rndNumber2, rndNumber3;
 
-        //btn.prop("disabled", true);
-        //for (let i = 0; i <= 3; i++) {
-
-        for (let i = 0; i < 100; i++) {
-            let rndNumber1 = math.random(0, 20);
+        for (let i = 0; i < 1000; i++) {
+            rndNumber1 = math.random(lambdaMin, setLambda1);
             for (let j = 0; j < 1; j++) {
-                let rndNumber2 = math.random(0, 36);
+                rndNumber2 = math.random(lambdaMin, setLambda2);
                 for (let k = 0; k < 1; k++) {
-                    let rndNumber3 = math.random(0, 36);
-                    func = a1 * math.exp(-rndNumber1 * t) - a2 * math.exp(-rndNumber2 * t) - a3 * math.exp(-rndNumber3 * t);
+                    rndNumber3 = math.random(lambdaMin, setLambda3);
+                    func = a1 * math.exp(-rndNumber1 * t) + a2 * math.exp(-rndNumber2 * t) + a3 * math.exp(-rndNumber3 * t);
                     let calculated = math.eval(func);
-                    // if ((calculated > 0) && (calculated < 1)) {
-                    let newRow1 = "<tr class='generated'>" + "<td>" +
-                        rndNumber1 + "</td>" +
-                        "<td>" + rndNumber2 +
-                        "</td>" +
-                        "<td>" +
-                        rndNumber3 + "</td>" + "<td>" + calculated + "</td>" +
-                        "</tr>";
-                    generate.after(newRow1);
-                    dataBase.push({
-                        "X1": rndNumber1,
-                        "X2": rndNumber2,
-                        "X3": rndNumber3,
-                        "label": "point" + i
-                    });
-                    //}
+                    if ((calculated > 0) && (calculated < 1e-10)) {
+                        let newRow1 = "<tr class='generated'>" + "<td>" +
+                            rndNumber1 + "</td>" +
+                            "<td>" + rndNumber2 +
+                            "</td>" +
+                            "<td>" +
+                            rndNumber3 + "</td>" + "<td>" + calculated + "</td>" +
+                            "</tr>";
+                        generate.after(newRow1);
+                        dataBase.push({
+                            "lambda1": rndNumber1,
+                            "lambda2": rndNumber2,
+                            "lambda3": rndNumber3,
+                            "label": "point" + i
+                        });
+                    }
                 }
             }
-        }
-
-
+        };
 
         function ternaryPlot(selector, userOpt) {
-
             let plot = {
                 dataset: []
             };
@@ -128,7 +135,7 @@ $(window).on("load", function () {
                     let coord2 = coord([v, 0 - v, -50]);
                     let coord3 = coord([-50, 0 - v, v]);
                     let coord4 = coord([0 - v, -50, v]);
-
+                    
                     axes.append("line")
                         .attr(lineAttributes(coord1, coord2))
                         .classed('a-axis minor-tick', true);
@@ -148,7 +155,7 @@ $(window).on("load", function () {
                 let coord2 = coord([v, 0 - v, -50]);
                 let coord3 = coord([-50, 0 - v, v]);
                 let coord4 = coord([0 - v, -50, v]);
-
+                
                 axes.append("line")
                     .attr(lineAttributes(coord1, coord2))
                     .classed('a-axis tick', true);
@@ -223,13 +230,15 @@ $(window).on("load", function () {
                     pos[1] = corners[0][1] * a + corners[1][1] * b + corners[2][1] * c;
                 }
                 return pos;
+                
             }
 
             plot.data = function (data, accessor, bindBy) { //bind by is the dataset property used as an id for the join
                 plot.dataset = data;
-
+                console.log('data: ' + data);
                 let circles = svg.selectAll("circle")
                     .data(data.map(function (d) {
+                        
                         return coord(accessor(d));
                     }), function (d, i) {
                         if (bindBy) {
@@ -241,21 +250,24 @@ $(window).on("load", function () {
                 circles.enter().append("circle");
 
                 circles.transition().attr("cx", function (d) {
+                    
                     return d[0];
                 })
                     .attr("cy", function (d) {
+                        
                         return d[1];
                     })
                     .attr("r", 5);
-
+                    
                 return this;
-            }
+            };
+
             plot.getPosition = coord;
             plot.getTripple = function (x, y) {
                 //TODO, get percentages for a give x, y
             }
             return plot;
-        }
+        };
 
 
         //ACTIVATE
@@ -267,13 +279,13 @@ $(window).on("load", function () {
                 bottom: 10,
                 right: 50
             },
-            axis_labels: ['X1', 'X2', 'X3'],
+            axis_labels: ['lambda1', 'lambda2', 'lambda3'],
             axis_ticks: d3.range(-50, 51, 10),
             minor_axis_ticks: d3.range(-50, 51, 5)
-        }
+        };
 
-        ternaryPlot('#plot', plot_opts).data(dataBase, function (d) { return [d.X1, d.X2, d.X3] }, 'label');
-
+        ternaryPlot('#plot', plot_opts).data(dataBase, function (d) { return [d.lambda1, d.lambda2, d.lambda3, d.label]}, 'label' );
+        
         event.stopPropagation();
     });
 });
